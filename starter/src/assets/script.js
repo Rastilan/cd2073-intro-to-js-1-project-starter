6c551aee36a9f9d17eb2fd96626cfc2e41a0116b
Rastilan <christoperbhall@gmail.com>
2024-08-11 21:35:06 -0400

    added functions for adding products to cart, increase and decreasing quantity, and removing the product

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,38 +1,73 @@
-/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+let products = [
+  {
+    name: "Cherry",
+    price: 3,
+    quantity: 0,
+    productId: 1,
+    image: "/images/cherry.jpg"
+  },
+  {
+    name: "Orange",
+    price: 2,
+    quantity: 0,
+    productId: 2,
+    image: "/images/orange.jpg"
+  },  
+  {
+    name: "Strawberry",
+    price: 5,
+    quantity: 0,
+    productId: 3,
+    image: "/images/strawberry.jpg"
+  }
+];
 
-/* Create 3 or more product objects using object literal notation 
-   Each product should include five properties
-   - name: name of product (string)
-   - price: price of product (number)
-   - quantity: quantity in cart should start at zero (number)
-   - productId: unique id for the product (number)
-   - image: picture of product (url string)
-*/
+let cart = [];
 
-/* Images provided in /images folder. All images from Unsplash.com
-   - cherry.jpg by Mae Mu
-   - orange.jpg by Mae Mu
-   - strawberry.jpg by Allec Gomes
-*/
+function addProductToCart(productId) {
+  let product = products.find((product) => product.productId === productId); // find product info in products array. 
+  if(product){
+    if (product.quantity === 0) {
+      cart.push(product); // adds product if it doesn't currently exist in the cart
+    }
+      product.quantity++; //included here to increase quanitity of product found or not
+  } else {
+    console.log(`Error: Product with ID ${product} is not a valid product`) //Product added to cart is not found in the products array
+  }
+}
 
-/* Declare an empty array named cart to hold the items in the cart */
+function increaseQuantity(productId){
+  let product = products.find((product) => product.productId === productId);
+  if(product){
+    product.quantity++;
+  } else {
+    console.log(`Error: Product with ID ${product} is not a valid product`) //Product added to cart is not found in the products array
+  }
+}
 
-/* Create a function named addProductToCart that takes in the product productId as an argument
-  - addProductToCart should get the correct product based on the productId
-  - addProductToCart should then increase the product's quantity
-  - if the product is not already in the cart, add it to the cart
-*/
+function decreaseQuantity(productId){
+  let product = products.find((product) => product.productId === productId);
+  if(product){
+    if(product.quantity === 1){
+      cart = cart.filter((product) => product.productId !== productId)
+    } else {
+      product.quantity--;
+    }
+  } else {
+    console.log(`Error: Product with ID ${product} is not a valid product`) //Product added to cart is not found in the products array
+  }
 
-/* Create a function named increaseQuantity that takes in the productId as an argument
-  - increaseQuantity should get the correct product based on the productId
-  - increaseQuantity should then increase the product's quantity
-*/
+}
 
-/* Create a function named decreaseQuantity that takes in the productId as an argument
-  - decreaseQuantity should get the correct product based on the productId
-  - decreaseQuantity should decrease the quantity of the product
-  - if the function decreases the quantity to 0, the product is removed from the cart
-*/
+function removeProductFromCart(productId){
+  let product = products.find((product) => product.productId === productId);
+  if(product){
+    product.quantity = 0;
+    cart = cart.filter((product) => product.productId !== productId);
+  } else {
+    console.log(`Error: Product with ID ${product} is not a valid product`) //Product added to cart is not found in the products array
+  }
+}
 
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
